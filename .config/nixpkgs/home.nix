1a16eecc4056cafc24da6500808fecea05deb598
Sergey Mishin <sergei.a.mishin@gmail.com>
2020-08-01 17:14:43 +0300

    Initial commit

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,40 @@
+
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
+
+  home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
+
+  home.packages = [
+    pkgs.htop
+    pkgs.konsole
+    pkgs.zsh
+  ];
+
+  # programs.zsh = {
+  #   enable = true;
+  # };
+
+  programs.zsh.enable = true;
+
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    theme = "robbyrussell";
+    plugins = [ "git" "sudo" ];
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.03";
+}
