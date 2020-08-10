3a14e174814c41d0ae1bd6384efe19f1c9c6b7dd
Sergey Mishin <sergei.a.mishin@gmail.com>
2020-08-11 00:02:37 +0300

    Fix indentation

--- configuration.nix
+++ configuration.nix
@@ -186,7 +186,7 @@
           i3status # gives you the default i3 status bar
           i3lock #default i3 screen locker
           i3blocks #if you are planning on using i3blocks over i3status
-    	    i3status-rust
+          i3status-rust
         ];
       };
     };
